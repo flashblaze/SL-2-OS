18504e404475d4e07bc0f4abea45daacb78cd59a
FlashBlaze <neerajlagwankar@gmail.com>
2017-09-10 23:39:18 +0530

    Mul

--- Assignment-3/matrix_mul.c
+++ Assignment-3/matrix_mul.c
@@ -3,6 +3,7 @@
 
 typedef struct {
 	
+	int i, j;
 	int row, col;
 	int **m1, **m2, **m3;
 }mat;
@@ -38,7 +39,52 @@ void display(int **m1, int row, int col) {
 	}
 }
 
+void threads(void *mat) {
+  mat *matrix = (struct mat *) matrix;
+  int k;
+  *(*((* matrix).m3 + (* matrix).i) + (* matrix).j) = 0;
+  for (k = 0;k < (* matrix).cols;k++) {
+    *(*((* matrix).m3 + (* matrix).i) + (* matrix).j) +=
+    *(*((* matrix).m1 + (* matrix).i) + k) *
+    *(*((* matrix).m2 + k) + (* matrix).j);
+  }
+  pthread_exit(NULL);
+}
+
 int main () {
 
+	int i, j;
+  	int row, col;
+  	int **m1, **m2, **m3;
+  	int threadn;
+  	pthread_t *threadm;
+  	printf("Enter the Number of Rows and Columns\n");
+  	scanf("%d%d",&row,&col);
+  	
+  	printf("Enter Elements of Array 1\n");
+  	m1 = accept(row, col);
+  	printf("Enter Elements of Array 2\n");
+  	m2 = accept(row, col);
+  	m3 = allocate(row, col);
+  	
+  	threadn = 0;
+  	threadm = (pthread_t *) malloc(sizeof(pthread_t) * (row * col));
+  	for (i = 0; i < row; i++) {
+	    for (j = 0; j < cols; j++) {
+      	mat *matrix = (struct mat *) malloc(sizeof(struct args));
+      	(* matrix).row = row;
+      	(* matrix).col = col;
+      	(* matrix).m1 = m1;
+      	(* matrix).m2 = m2;
+      	(* matrix).m3 = m3;
+      	(* matrix).i = i;
+      	(* matrix).j = j;
+      	pthread_create(&threadm[threadn++], NULL, threads, (void *) matrix);
+    	}
+  	}
+  	for (j = 0;j < threadn;j++) {
+	    pthread_join(threadm[j], NULL);
+  	}
+  	display(c, row, col);
 	return 0;
 }
